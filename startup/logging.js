cdad2e129bd2a0e1005cb2c4447668282517b5ae
pablomora <pmora1990@gmail.com>
2020-05-13 12:03:37 -0600

    Fix some issues on logging.js

--- startup/logging.js
+++ startup/logging.js
@@ -1,11 +1,9 @@
 const winston = require('winston');
-// const moment = require('moment');
 const { createLogger, format, transports } = require('winston');
 const { combine, timestamp, label, printf, prettyPrint } = format;
 const path = require('path');
 const logPath = __dirname;
 const tsFormat = () => (new Date().toISOString());
-
 
 const myFormat = printf(({ level, message, label, timestamp }) => {
     return `${timestamp} [${label}] ${level}: ${message}`;
@@ -16,11 +14,13 @@ const errorLog = createLogger({
     format: combine(
         label({ label: 'errorLog' }),
         timestamp(),
-        myFormat
+        prettyPrint(),
+        // myFormat
     ),
     transports: [
         new winston.transports.File({
-            filename: path.join(logPath, 'errors.log'),
+            filename: path.join(logPath, '/logs/errors.log'),
+            'timestamp': true,
             timestamp: tsFormat,
             level: 'info'
         })
@@ -31,7 +31,7 @@ const accessLog = createLogger({
     level: 'info',
     format: combine(
         label({ label: 'accessLog' }),
-        // timestamp(),
+        timestamp(),
         prettyPrint(),
         // myFormat,
     ),

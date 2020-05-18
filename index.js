46ce42829aa95c05a91da4bfc140cd6b26d18d44
pablomora <pmora1990@gmail.com>
2020-05-18 12:21:04 -0600

    Create main layout and navbar partial

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
+const path = require('path');
 const app = express();
 
 const logger = require('./startup/logging');
 require('./startup/routes')(app);
 require('./startup/prod')(app);
 
-app.engine('handlebars',exphbs());
+app.engine('handlebars',exphbs({
+    defaultLayout: 'main', 
+    layoutsDir: path.join(__dirname, 'views/mainLayout')
+}));
 app.set('view engine', 'handlebars');
 
 const port = process.env.PORT || 3000;
--- routes/home.js
+++ routes/home.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.render('index', { title: 'eCommerce-NodeJS', message: 'Hello world'})
+    res.render('index', { title: 'Index page', message: 'Index page'})
 });
 
 module.exports = router;

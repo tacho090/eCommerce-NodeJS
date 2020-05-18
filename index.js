928867015a3ea256128612b58b5af5df54fa970d
pablomora <pmora1990@gmail.com>
2020-05-18 11:36:27 -0600

    Install express-handlebars package and create main and layout views

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
 const app = express();
 
 const logger = require('./startup/logging');
 require('./startup/routes')(app);
 require('./startup/prod')(app);
 
-app.set('view engine', 'pug');
-app.set('views', './views');
+app.engine('handlebars',exphbs());
+app.set('view engine', 'handlebars');
 
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {

86c19c142b867ee47f47e04e3d1b88fe93862da2
Pablo Mora <pmora1990@gmail.com && git config --global user.name Pablo>
2020-05-19 12:13:48 -0600

    Begin database product seeding

--- models/product.js
+++ models/product.js
@@ -1,11 +1,19 @@
 const mongoose = require('mongoose');
 
 const Product = mongoose.model('Product', new mongoose.Schema({
+    imagePath: {
+        type: String,
+        required: true
+    },
     name: {
         type: String,
         required: true,
         minlength: 5,
         maxlength: 50
+    },
+    description: {
+        type: String,
+        required: true
     },
     price: {
         type: Number,
---
+++ seed/productSeeder.js
@@ -0,0 +1,8 @@
+const Product = require('../models/product');
+
+const products = [new Product({
+    imagePath: 'https://www.srichinmoycentre.org/files/Members/toshala/puppy-small.jpg',
+    title: 'Doge',
+    description: 'awesome Doge',
+    price: 10
+})];

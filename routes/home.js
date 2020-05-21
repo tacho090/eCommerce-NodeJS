16a246288cc4ef97220f64f7aa05b23a03ff0e4e
pablomora <pmora1990@gmail.com>
2020-05-20 21:10:09 -0600

    Succesfully serve mongoDB data to page

--- routes/home.js
+++ routes/home.js
@@ -1,11 +1,21 @@
+const { Product } = require('../models/product');
 const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.render('shop/index', {
-        title: 'Index page',
-        message: 'Index page'
-    });
+    Product.find({}).lean()
+        .exec(function(err, docs) {
+            res.render('shop/index', {
+                title: 'eCommerce NodeJS',
+                products: docs
+            });
+        });
+    // Product.find(function(err, docs) {
+    //     res.render('shop/index', {
+    //         title: 'eCommerce NodeJS',
+    //         products: docs
+    //     });
+    // });
 });
 
 module.exports = router;

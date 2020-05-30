bd6940c6b75098b773215eed283f9cd60e07ddc1
Pablo Mora <pmora1990@gmail.com && git config --global user.name Pablo>
2020-05-30 16:05:30 -0600

    Validation logic for user model

--- startup/routes.js
+++ startup/routes.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const home = require('../routes/home');
 const products = require('../routes/products');
+const users = require('../routes/users');
 
 module.exports = function(app) {
     app.use(express.json());
     app.use('/', home);
     app.use('/api/products', products);
+    app.use('/api/users', users);
 }

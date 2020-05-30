83798ddf49b631d9d03667c148cddc1ef1d5451a
Pablo Mora <pmora1990@gmail.com && git config --global user.name Pablo>
2020-05-30 13:50:33 -0600

    Add user Model

---
+++ models/user.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+
+const User = mongoose.model('User', new mongoose.Schema({
+    email: { type: String, required: true },
+    password: { type: String, required: true }
+}));
+
+exports.User = User;

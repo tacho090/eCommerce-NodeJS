f71e458877818bfbb75963d2edd7ff814fa0597a
Pablo Mora <pmora1990@gmail.com && git config --global user.name Pablo>
2020-05-30 16:40:01 -0600

    hash user password

--- routes/users.js
+++ routes/users.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcrypt');
 const { User, validate } = require('../models/user');
 const express = require('express');
 const router = express.Router();
@@ -14,6 +15,8 @@ router.post('/',async (req, res) => {
         email: req.body.email,
         password: req.body.password 
     });
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(user.password, salt);
 
     await user.save();
     res.send(user);
--- startup/routes.js
+++ startup/routes.js
@@ -2,10 +2,12 @@ const express = require('express');
 const home = require('../routes/home');
 const products = require('../routes/products');
 const users = require('../routes/users');
+const auth = require('../routes/auth');
 
 module.exports = function(app) {
     app.use(express.json());
     app.use('/', home);
     app.use('/api/products', products);
     app.use('/api/users', users);
+    app.use('/api/auth', auth);
 }

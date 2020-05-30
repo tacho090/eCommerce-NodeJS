19198ec5ffa985662a0c0c9bee355e5a51aebb26
Pablo Mora <pmora1990@gmail.com && git config --global user.name Pablo>
2020-05-30 15:55:54 -0600

    Refactor User Model

--- models/user.js
+++ models/user.js
@@ -1,8 +1,39 @@
+const Joi = require('joi');
 const mongoose = require('mongoose');
+const minL = 5;
+const maxN = 50;
+const maxE = 255;
 
 const User = mongoose.model('User', new mongoose.Schema({
-    email: { type: String, required: true },
-    password: { type: String, required: true }
+    name: {
+        type: String,
+        required: true,
+        minlength: minL,
+        maxlength: maxN
+    },
+    email: { 
+        type: String, 
+        required: true,
+        minlength: minL,
+        maxlength: maxE,
+        unique: true 
+    },
+    password: { 
+        type: String, 
+        required: true,
+        minlength: minL,
+        maxlength: 1024,
+    }
 }));
 
+function validateUser(user) {
+    const schema = {
+        name: Joi.string().min(minL).max(maxN).required(),
+        email: Joi.string().min(minL).max(maxE).required().email(),
+        password: Joi.string().min(minL).max(maxE).required()
+    };
+    return Joi.validate(user, schema);
+}
+
 exports.User = User;
+exports.validate = validateUser;

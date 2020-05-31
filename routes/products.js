acf9998ad4b123297834d74e17153ce0efd208fe
Pablo Mora <pmora1990@gmail.com && git config --global user.name Pablo>
2020-05-30 18:17:29 -0600

    Get user using jwt

--- routes/products.js
+++ routes/products.js
@@ -1,8 +1,9 @@
+const auth = require('../middleware/auth');
 const { Product } = require('../models/product');
 const express = require('express');
 const router = express.Router();
 
-router.post('/',async (req, res) => {
+router.post('/', auth, async (req, res) => {
     let product = new Product({
         imagePath: req.body.imagePath,
         name: req.body.name,
--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,14 @@
-const jwt = require('jsonwebtoken');
-const config = require('config');
+const auth = require('../middleware/auth');
 const _= require('lodash');
 const bcrypt = require('bcrypt');
 const { User, validate } = require('../models/user');
 const express = require('express');
 const router = express.Router();
+
+router.get('/me', auth, async (req, res) => {
+    const user = await User.findById(req.user._id).select('-password');
+    res.send(user);
+})
 
 router.post('/',async (req, res) => {
     const { error } = validate(req.body);

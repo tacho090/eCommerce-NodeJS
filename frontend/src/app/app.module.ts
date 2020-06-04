580862df877ade491749408929af5e739d8b9e74
Pablo Mora <pmora1990@gmail.com && git config --global user.name Pablo>
2020-06-03 18:09:05 -0600

    Implement basic routing

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,25 +2,35 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ProductsComponent } from './components/products/products.component';
 import { HttpClientModule } from '@angular/common/http';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { ShoppingCartComponent } from './components/shopping-cart/shopping-cart.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductsComponent,
     NavBarComponent,
-    ShoppingCartComponent
+    ShoppingCartComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent},
+      { path: 'products', component: ProductsComponent },
+      { path: 'shopping-cart', component: ShoppingCartComponent },
+      { path: '**', component: NotFoundComponent }
+    ]),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
